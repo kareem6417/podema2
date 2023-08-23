5cdb61575fce304d6196b4aa385e0d22d5042b8f
kareem6417 <132553583+kareem6417@users.noreply.github.com>
2023-08-23 17:01:49 +0700

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -7,7 +7,16 @@ if (isset($_SESSION['login_attempt'])) {
     $_SESSION['login_attempt'] = 1;
 }
 
-require_once 'config.php';
+$hostname = "mandiricoal.co.id";
+$username = "mandiricoal";
+$password = "Mandiricoal2022!";
+$database = "podema";
+
+$conn_podema = mysqli_connect($hostname, $username, $password, $database);
+
+if (!$conn_podema) {
+    die("Koneksi database userdata gagal: " . mysqli_connect_error());
+}
 
 if (isset($_POST['reset'])) {
     unset($_POST['username']);
