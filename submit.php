72e61208daca8e3997723df03d7f072fe92aa551
kareem6417 <132553583+kareem6417@users.noreply.github.com>
2023-08-28 10:37:41 +0700

    Update submit.php

--- submit.php
+++ submit.php
@@ -48,7 +48,7 @@ if ($conn->query($sql) === TRUE) {
     } else {
         if (move_uploaded_file($temp_name, $path_filename_ext)) {
             echo "File Anda berhasil diunggah.";
-            header('Location: view.php');
+            echo '<meta http-equiv="refresh" content="0;url=view.php">';
             exit();
         } else {
             echo "Terjadi kesalahan saat mengunggah file.";
