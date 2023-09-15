61e6e49442d518af429fc7436d7703a4b7cc0148
kareem6417 <132553583+kareem6417@users.noreply.github.com>
2023-09-15 14:32:30 +0700

    Update ins.js

--- js/ins.js
+++ js/ins.js
@@ -1,4 +1,4 @@
-document.getElementById('jenis').addEventListener('change', function() {
+    document.getElementById('jenis').addEventListener('change', function() {
         var selectedJenis = this.value;
         var infoDiv = document.getElementById('infoDiv');
         var casingDiv = document.getElementById('casingDiv');
@@ -16,7 +16,11 @@ document.getElementById('jenis').addEventListener('change', function() {
         var inkpadDiv = document.getElementById('inkpadDiv');
         var rekomDiv = document.getElementById('rekomDiv');
 
-        if(selectedJenis === 'Laptop') {
+        // Sembunyikan semua elemen terlebih dahulu
+        hideAllDivs();
+
+        // Tampilkan elemen berdasarkan jenis perangkat yang dipilih
+        if (selectedJenis === 'Laptop') {
             infoDiv.style.display = 'block';
             casingDiv.style.display = 'block';
             layarDiv.style.display = 'block';
@@ -30,26 +34,28 @@ document.getElementById('jenis').addEventListener('change', function() {
             portDiv.style.display = 'block';
             audioDiv.style.display = 'block';
             softwareDiv.style.display = 'block';
+            inkpadDiv.style.display = 'none';
             rekomDiv.style.display = 'block';
-        } else if(selectedJenis === 'PC Desktop') {
+        } else if (selectedJenis === 'PC Desktop') {
             infoDiv.style.display = 'block';
-            casingDiv.style.display = 'none';
-            layarDiv.style.display = 'block';
+            casingDiv.style.display = 'block'; // Tampilkan casing untuk PC Desktop
+            layarDiv.style.display = 'block'; // Tampilkan layar untuk PC Desktop
             engselDiv.style.display = 'none';
-            keyboardDiv.style.display = 'block';
+            keyboardDiv.style.display = 'block'; // Tampilkan keyboard untuk PC Desktop
             touchpadDiv.style.display = 'none';
             bootingDiv.style.display = 'block';
             multiDiv.style.display = 'block';
             tampungDiv.style.display = 'none';
             isiDiv.style.display = 'none';
-            portDiv.style.display = 'block';
+            portDiv.style.display = 'block'; // Tampilkan port untuk PC Desktop
             audioDiv.style.display = 'none';
-            softwareDiv.style.display = 'block';
+            softwareDiv.style.display = 'block'; // Tampilkan software untuk PC Desktop
+            inkpadDiv.style.display = 'none';
             rekomDiv.style.display = 'block';
-        } else if(selectedJenis === 'Monitor') {
+        } else if (selectedJenis === 'Monitor') {
             infoDiv.style.display = 'block';
-            casingDiv.style.display = 'none';
-            layarDiv.style.display = 'block';
+            casingDiv.style.display = 'block'; // Tampilkan casing untuk Monitor
+            layarDiv.style.display = 'block'; // Tampilkan layar untuk Monitor
             engselDiv.style.display = 'none';
             keyboardDiv.style.display = 'none';
             touchpadDiv.style.display = 'none';
@@ -60,11 +66,12 @@ document.getElementById('jenis').addEventListener('change', function() {
             portDiv.style.display = 'none';
             audioDiv.style.display = 'none';
             softwareDiv.style.display = 'none';
+            inkpadDiv.style.display = 'none';
             rekomDiv.style.display = 'block';
-        } else {
+        } else if (selectedJenis === 'Printer') {
             infoDiv.style.display = 'block';
-            casingDiv.style.display = 'block';
-            layarDiv.style.display = 'none';
+            casingDiv.style.display = 'block'; // Tampilkan casing untuk Printer
+            layarDiv.style.display = 'block'; // Tampilkan layar untuk Printer
             engselDiv.style.display = 'none';
             keyboardDiv.style.display = 'none';
             touchpadDiv.style.display = 'none';
@@ -75,7 +82,17 @@ document.getElementById('jenis').addEventListener('change', function() {
             portDiv.style.display = 'none';
             audioDiv.style.display = 'none';
             softwareDiv.style.display = 'none';
-            inkpadDiv.style.display = 'block';
+            inkpadDiv.style.display = 'block'; // Tampilkan inkpad untuk Printer
             rekomDiv.style.display = 'block';
         }
     });
+
+    function hideAllDivs() {
+        var divs = document.querySelectorAll('.content > div');
+        for (var i = 0; i < divs.length; i++) {
+            divs[i].style.display = 'none';
+        }
+    }
+
+    // Panggil fungsi untuk menyembunyikan semua elemen saat halaman pertama kali dimuat
+    hideAllDivs();
