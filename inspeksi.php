5b2dfc211721d43c3dba05d509e80dc5dbe6e056
kareem6417 <132553583+kareem6417@users.noreply.github.com>
2023-09-18 15:44:34 +0700

    Update inspeksi.php

--- inspeksi.php
+++ inspeksi.php
@@ -304,41 +304,36 @@ foreach ($users as $user) {
             <br>
             <script>
                 document.addEventListener('DOMContentLoaded', function() {
-                    var jenisPerangkat = document.getElementById('jenis').value;
-                    showHideElements(jenisPerangkat);
-
                     document.getElementById('jenis').addEventListener('change', function() {
                         var jenisPerangkat = this.value;
                         showHideElements(jenisPerangkat);
                     });
 
                     function showHideElements(jenisPerangkat) {
-                        var elementsToShow = [];
-                        var elementsToHide = [];
-
+                        var allElements = ['informasi_keluhan', 'casing', 'layar', 'engsel', 'keyboard', 'touchpad', 'booting', 'multitasking', 'tampung', 'isi', 'port', 'software', 'audio', 'hasil_pemeriksaan', 'screenshot', 'rekomendasi', 'ink_pad'];
+                        var elementsToShow = ['informasi_keluhan', 'hasil_pemeriksaan', 'screenshot', 'rekomendasi'];
+                        
                         if (jenisPerangkat === 'Laptop') {
-                            elementsToShow = ['casing_lap', 'layar_lap', 'engsel_lap', 'keyboard_lap', 'touchpad_lap', 'booting_lap', 'multi_lap', 'tampung_lap', 'isi_lap', 'port_lap', 'audio_lap', 'hasil_pemeriksaan', 'screenshot', 'rekomendasi', 'upload_file'];
+                            elementsToShow = elementsToShow.concat(['casing', 'layar', 'engsel', 'keyboard', 'touchpad', 'booting', 'multitasking', 'tampung', 'isi', 'port', 'software', 'audio']);
                         } else if (jenisPerangkat === 'PC Desktop') {
-                            elementsToShow = ['casing_lap', 'layar_lap', 'keyboard_lap', 'booting_lap', 'multi_lap', 'port_lap', 'audio_lap', 'hasil_pemeriksaan', 'screenshot', 'rekomendasi', 'upload_file'];
-                        } else if (jenisPerangkat === 'Monitor' || jenisPerangkat === 'Printer') {
-                            elementsToShow = ['casing_lap', 'layar_lap', 'hasil_pemeriksaan', 'screenshot', 'rekomendasi', 'upload_file'];
-                            if (jenisPerangkat === 'Printer') {
-                                elementsToShow.push('ink_pad');
-                            }
+                            elementsToShow = elementsToShow.concat(['casing', 'layar', 'keyboard', 'booting', 'multitasking', 'port', 'audio', 'software']);
+                        } else if (jenisPerangkat === 'Monitor') {
+                            elementsToShow = elementsToShow.concat(['casing', 'layar']);
+                        } else if (jenisPerangkat === 'Printer') {
+                            elementsToShow = elementsToShow.concat(['casing', 'ink_pad']);
                         }
 
-                        var allElements = ['casing_lap', 'layar_lap', 'engsel_lap', 'keyboard_lap', 'touchpad_lap', 'booting_lap', 'multi_lap', 'tampung_lap', 'isi_lap', 'port_lap', 'software_lap', 'audio_lap', 'hasil_pemeriksaan', 'screenshot', 'rekomendasi', 'upload_file', 'ink_pad'];
-                        elementsToHide = allElements.filter(element => !elementsToShow.includes(element));
-
-                        elementsToHide.forEach(function(elementId) {
-                            document.getElementById(elementId).style.display = 'none';
-                        });
-
-                        elementsToShow.forEach(function(elementId) {
-                            document.getElementById(elementId).style.display = 'block';
+                        allElements.forEach(function(elementId) {
+                            var element = document.getElementById(elementId);
+                            if (elementsToShow.includes(elementId)) {
+                                element.style.display = 'block';
+                            } else {
+                                element.style.display = 'none';
+                            }
                         });
                     }
                 });
+
             </script>
             <input type="submit" value="Submit">
             <input type="reset" value="Reset">
