089ddf3eae36ce4b9af37af8f2e77c3a30d173fe
kareem6417 <132553583+kareem6417@users.noreply.github.com>
2023-09-21 11:19:32 +0700

    Update submit_inspeksi.php

--- submit_inspeksi.php
+++ submit_inspeksi.php
@@ -114,22 +114,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     
         // Proses upload screenshot
-        $targetDirectory = "/dev-podema/screenshot/";
-        $targetFile = $targetDirectory . basename($_FILES["screenshot"]["name"]);
+        $target_dir = "dev-podema/screenshot/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
         $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-        // Jika file screenshot diunggah
-        if ($_FILES["screenshot"]["error"] == UPLOAD_ERR_OK) {
-            $tmp_name = $_FILES["screenshot"]["tmp_name"];
-            $name = $_FILES["screenshot"]["name"];
-            move_uploaded_file($tmp_name, "$targetDirectory/$name");
-            echo "File screenshot berhasil diunggah.";
-        } else {
-            echo "Terjadi kesalahan saat mengunggah screenshot.";
+        // Check if image file is a actual image or fake image
+        if (isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if ($check !== false) {
+                echo "File adalah gambar - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File bukan gambar.";
+                $uploadOk = 0;
+            }
         }
-            header("Location: viewinspeksi.php");
-            exit();
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Maaf, file sudah ada.";
+            $uploadOk = 0;
+        }
+
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Maaf, ukuran file terlalu besar.";
+            $uploadOk = 0;
+        }
+
+        // Allow certain file formats
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif") {
+            echo "Maaf, hanya file JPG, JPEG, PNG & GIF yang diperbolehkan.";
+            $uploadOk = 0;
+        }
+
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Maaf, file Anda tidak dapat diunggah.";
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "Koneksi Berhasil! File Anda berhasil diunggah.";
+                header("Location: viewinspeksi.php");
+                exit();
+            } else {
+                echo "Maaf, terjadi kesalahan saat mengunggah file.";
+            }
+        }
     }    
 }
 
