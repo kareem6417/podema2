d0d1ca06859c85503de768bb4460e39e2b8bc57c
kareem6417 <132553583+kareem6417@users.noreply.github.com>
2023-10-03 14:52:06 +0700

    Update downloadinspeksi.php

--- downloadinspeksi.php
+++ downloadinspeksi.php
@@ -20,7 +20,6 @@ $result = $query->get_result();
 if ($result) {
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        $jenis = $row['jenis'];
     } else {
         die("Tidak ada data ditemukan di tabel form_inspeksi.");
     }
@@ -34,6 +33,22 @@ $createDate = date('m/Y', strtotime($row['date']));
 
 $nomorInspeksi = sprintf("%03d", $runningNumber) . "/MIP/INS/" . $createDate;
 
+$jenis = $_POST['jenis'];
+$informasi_keluhan = $_POST['informasi_keluhan'];
+$casing_lap = $_POST['casing_lap'];
+$layar_lap = $_POST['layar_lap'];
+$engsel_lap = $_POST['engsel_lap'];
+$keyboard_lap = $_POST['keyboard_lap'];
+$touchpad_lap = $_POST['touchpad_lap'];
+$booting_lap = $_POST['booting_lap'];
+$multi_lap = $_POST['multi_lap'];
+$tampung_lap = $_POST['tampung_lap'];
+$isi_lap = $_POST['isi_lap'];
+$port_lap = $_POST['port_lap'];
+$audio_lap = $_POST['audio_lap'];
+$software_lap = $_POST['software_lap'];
+$hasil_pemeriksaan = $_POST['hasil_pemeriksaan'];
+$rekomendasi = $_POST['rekomendasi'];
 
 $pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8', false);
 $pdf->SetMargins(10, 10, 10);
@@ -97,6 +112,8 @@ class MYPDF extends TCPDF {
     }
 }
 
+
+
 $pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
@@ -155,117 +172,59 @@ $pdf->SetFont('helvetica', '', 11);
 $pdf->Cell($cellWidth * 2, 10, $row['lokasi'], 1, 1, 'L', false);
 $pdf->Ln(10);
 
-$html .= '</tbody>';
-$html .= '</table>';
-$pdf->writeHTML($html, true, false, true, false, '');
-$pdf->Ln();
+if ($jenis == "Laptop") {
+    // Tambahkan elemen khusus untuk Laptop
+    $pdf->SetFont('helvetica', 'B', 12);
+    $pdf->Cell(0, 10, 'Pemeriksaan Laptop', 0, true, 'C', 0, '', 0, false, 'M', 'M');
 
-$html = '<table style="width: 100%; border-collapse: collapse; border: none;">';
-$html .= '<thead>';
-$html .= '<tr>';
-$html .= '<th style="background-color: #FFE4B5; padding: 5px; font-weight: bold;" colspan="2">Informasi Keluhan/Permasalahan yang disampaikan:</th>';
-$html .= '</tr>';
-$html .= '</thead>';
-$html .= '<tbody>';
+    $pdf->SetFont('helvetica', '', 11);
+    $pdf->MultiCell(0, 10, 'Casing: ' . $casing_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Layar: ' . $layar_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Engsel: ' . $engsel_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Keyboard: ' . $keyboard_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Touchpad: ' . $touchpad_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Booting: ' . $booting_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Multi: ' . $multi_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Tampung: ' . $tampung_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Isi: ' . $isi_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Port: ' . $port_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Audio: ' . $audio_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Software: ' . $software_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+} else if ($jenis == "Desktop") {
+    // Tambahkan elemen khusus untuk Desktop
+    $pdf->SetFont('helvetica', 'B', 12);
+    $pdf->Cell(0, 10, 'Pemeriksaan Desktop', 0, true, 'C', 0, '', 0, false, 'M', 'M');
 
-$html .= '<tr><td style="padding: 5px; border: none;" colspan="2"></td></tr>';
+    $pdf->SetFont('helvetica', '', 11);
+    $pdf->MultiCell(0, 10, 'Casing: ' . $casing_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Layar: ' . $layar_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Keyboard: ' . $keyboard_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Booting: ' . $booting_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Multi: ' . $multi_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Port: ' . $port_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Audio: ' . $audio_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Software: ' . $software_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+} else if ($jenis == "Monitor") {
+    // Tambahkan elemen khusus untuk Monitor
+    $pdf->SetFont('helvetica', 'B', 12);
+    $pdf->Cell(0, 10, 'Pemeriksaan Monitor', 0, true, 'C', 0, '', 0, false, 'M', 'M');
 
-$complaints = explode("\n", $row['informasi_keluhan']);
-foreach ($complaints as $index => $complaint) {
-    $html .= '<tr>';
-    $html .= '<td style="padding: 5px; border: none;" colspan="2">' . nl2br($complaint) . '</td>';
-    $html .= '</tr>';
-    if ($index < count($complaints) - 1 || $index == count($complaints) - 1) {
-        $html .= '<tr>';
-        $html .= '<td style="padding: 0; border: none; border-top: 1px solid black; height: 100%;" colspan="2"></td>';
-        $html .= '</tr>';
-    }
+    $pdf->SetFont('helvetica', '', 11);
+    $pdf->MultiCell(0, 10, 'Casing: ' . $casing_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Layar: ' . $layar_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+} else if ($jenis == "Printer") {
+    // Tambahkan elemen khusus untuk Printer
+    $pdf->SetFont('helvetica', 'B', 12);
+    $pdf->Cell(0, 10, 'Pemeriksaan Printer', 0, true, 'C', 0, '', 0, false, 'M', 'M');
+
+    $pdf->SetFont('helvetica', '', 11);
+    $pdf->MultiCell(0, 10, 'Casing: ' . $casing_lap, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+    $pdf->MultiCell(0, 10, 'Ink Pad: ' . $ink_pad, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
 }
 
-$html .= '</tbody>';
-$html .= '</table>';
-$pdf->writeHTML($html, true, false, true, false, '');
-$pdf->Ln();
+$pdf->MultiCell(0, 10, 'Hasil Pemeriksaan: ' . $hasil_pemeriksaan, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
+$pdf->MultiCell(0, 10, 'Rekomendasi: ' . $rekomendasi, 0, 'L', false, 1, '', '', true, 0, false, true, 10, 'T');
 
-//hasil_pemeriksaan
-$html = '<table style="width: 100%; border-collapse: collapse; border: none;">';
-$html .= '<thead>';
-$html .= '<tr>';
-$html .= '<th style="background-color: #FFE4B5; padding: 5px; font-weight: bold;" colspan="2">Hasil Pemeriksaan:</th>';
-$html .= '</tr>';
-$html .= '</thead>';
-$html .= '<tbody>';
-$html .= '<tr><td style="padding: 5px; border: none;" colspan="2"></td></tr>'; 
-
-$complaints = explode("\n", $row['hasil_pemeriksaan']);
-foreach ($complaints as $index => $complaint) {
-    $html .= '<tr>';
-    $html .= '<td style="padding: 5px; border: none;" colspan="2">' . nl2br($complaint) . '</td>';
-    $html .= '</tr>';
-    if ($index < count($complaints) - 1 || $index == count($complaints) - 1) {
-        $html .= '<tr>';
-        $html .= '<td style="padding: 0; border: none; border-top: 1px solid black; height: 100%;" colspan="2"></td>';
-        $html .= '</tr>';
-    }
-}
-
-$html .= '</tbody>';
-$html .= '</table>';
-$pdf->writeHTML($html, true, false, true, false, '');
-$pdf->Ln();
-
-//rekomendasi
-$html = '<table style="width: 100%; border-collapse: collapse; border: none;">';
-$html .= '<thead>';
-$html .= '<tr>';
-$html .= '<th style="background-color: #FFE4B5; padding: 5px; font-weight: bold;" colspan="2">Rekomendasi:</th>';
-$html .= '</tr>';
-$html .= '</thead>';
-$html .= '<tbody>';
-$html .= '<tr><td style="padding: 5px; border: none;"></td></tr>';
-
-$complaints = explode("\n", $row['rekomendasi']);
-foreach ($complaints as $index => $complaint) {
-    $html .= '<tr>';
-    $html .= '<td style="padding: 5px; border: none;" colspan="2">' . nl2br($complaint) . '</td>';
-    $html .= '</tr>';
-    if ($index < count($complaints) - 1 || $index == count($complaints) - 1) {
-        $html .= '<tr>';
-        $html .= '<td style="padding: 0; border: none; border-top: 1px solid black; height: 1px;" colspan="2"></td>';
-        $html .= '</tr>';
-    }
-}
-
-$html .= '<tr><td style="padding: 5px; border: none; height: 5px;"></td></tr>'; 
-
-$html .= '</tbody>';
-$html .= '</table>';
-$pdf->writeHTML($html, true, false, true, false, '');
-$pdf->Ln();
-
-$pdf->SetFont('helvetica', 'B', 10);
-$location = '    Jakarta,';
-$currentDate = date('d F Y');
-$locationWidth = $pdf->GetStringWidth($location);
-$dateWidth = $pdf->GetStringWidth($currentDate);
-$totalWidth = $locationWidth + $dateWidth + 5;
-
-$pdf->SetX(10);
-$pdf->Cell($locationWidth, 5, $location, 0, 0, 'L');
-$pdf->Cell(1, 1, '', 0, 0, 'C');
-$pdf->Cell($dateWidth, 5, $currentDate, 0, 1, 'L');
-
-$pdf->Ln(20);
-
-$pdf->Cell(95, 10, '', 0, 0, 'C');
-$pdf->Cell(5, 10, '', 0, 0, 'C');
-$pdf->Cell(95, 10, '', 0, 1, 'C');
-
-$pdf->SetFont('helvetica', 'B', 12);
-$pdf->Cell(47.5, 10, 'Diperiksa Oleh', 'T', 0, 'L');
-$pdf->Cell(5, 10, '', 0, 0, 'C');
-$pdf->SetFont('helvetica', 'B', 12);
-$pdf->Cell(47.5, 10, 'Pengguna', 'T', 1, 'C');
 
 $filename = "Inspection-Devices.pdf";
 $pdf->Output($filename, 'D');
